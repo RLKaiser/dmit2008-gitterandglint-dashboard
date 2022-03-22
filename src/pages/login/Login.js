46275481b93ff668174662da14ab805822039285
Richelle Kaiser <kaiser.richelle@gmail.com>
2022-03-22 11:05:12 -0600

    Adding login authentication

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,39 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import { BiMessageError } from 'react-icons/bi';
 
-import { LoginPageStyles } from './styles';
+import { auth } from 'libs/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { LoginPageStyles, LoginPageContainer } from './styles';
 import { Input } from '../../ui/forms/input';
 import { Label } from '../../ui/forms/label';
 import { Button, Link } from '../../ui/buttons';
 import CrystalShopLogo from '../../static/CrystalShopLogo.png';
 
 function Login(props) {
-  let navigation = useNavigate();
+  const navigator = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const notify = (error) => toast.error(error.code,{
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    icon: <BiMessageError/>
+  });
 
   function onHandleSubmit(evt) {
     evt.preventDefault();
-    navigation('dashboard');
+    signInWithEmailAndPassword(auth, email, password)
+    .then(userCredential=>{
+      navigator('dashboard')
+    })
+    .catch(error=>{
+      notify(error);
+    })
   }
   return ( 
     <>
       <main>
         <LoginPageStyles>
-          <div>
-            <h1>Welcome Back!</h1>
-            <p>Sign in to Glitter & Glint Crystals Dashboard</p>
+        <ToastContainer/>
+          <LoginPageContainer>
+            <div>
+              <h1>Welcome Back!</h1>
+              <p>Sign in to Glitter & Glint Crystals Dashboard</p>
 
-            <form onSubmit={ onHandleSubmit }>
-              <Label>Email Address:</Label>
-              <Input type="text" required/>
-              <Label>Password:</Label>
-              <Input type="text" required/>
-              <Button>Sign In</Button>
-            </form>
-            <Link>Forgot Password</Link>
-          </div>
-          <div>
-            <img src={CrystalShopLogo} alt="Crystal shop logo" />
-          </div>
+              <form onSubmit={ onHandleSubmit }>
+                <Label>Email Address:</Label>
+                <Input type="text" required placeholder="janedo@gmail.com" onChange={ (evt)=>setEmail(evt.target.value) }/>
+                <Label>Password:</Label>
+                <Input type="text" required placeholder="account password" onChange={ (evt)=>setPassword(evt.target.value) }/>
+                <Button>Sign In</Button>
+              </form>
+              <Link>Forgot Password</Link>
+            </div>
+            <div>
+              <img src={CrystalShopLogo} alt="Crystal shop logo" />
+            </div>
+          </LoginPageContainer>
         </LoginPageStyles>
       </main>
     </>
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,10 +1,41 @@
 import styled from 'styled-components';
 
 const LoginPageStyles = styled.section`
+  max-width: 1400px;
+  margin: 0 auto;
+  position: relative;
+
+  .Toastify__toast {
+    position: absolute;
+    top: 0.5rem;
+    left: 4rem;
+    width: 300px;
+    border-radius: 3px;
+    border: 2px solid red;
+    background-color: #F9F9F9;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .Toastify__toast-body {
+      display: flex;
+      gap: 1rem;
+    }
+    .Toastify__close-button {
+      background-color: transparent;
+      width: 2rem;
+      border: none;
+    }
+  }
+`;
+
+const LoginPageContainer = styled.div`
   background-color: #ffffff;
-  margin: 1rem;
   border-radius: 3px;
-  
+  margin: 1rem;
+
   p {
     font-size: 1.25rem;
     margin-bottom: 3rem;
@@ -24,6 +55,7 @@ const LoginPageStyles = styled.section`
       max-height: 600px;
     }
   }
+
   @media only screen and (min-width: 850px) {
     display: flex;
     justify-content: space-between;
@@ -41,10 +73,6 @@ const LoginPageStyles = styled.section`
       padding: 1rem;
     }
   }
-  @media only screen and (min-width: 1400px) {
-    max-width: 1400px;
-    margin: 3rem auto;
-  }
 `;
 
-export { LoginPageStyles }
+export { LoginPageStyles, LoginPageContainer }
